d58c4dfce00bc3688e9bf80c928f3ea2147795a6
Shlok Bhakta <shlokbhakta1@gmail.com>
2025-08-01 00:24:21 -0500

    Create Ship Assets

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Game development environment with Godot and Aseprite";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Game engine
+            godot
+
+            # Pixel art editor
+            aseprite
+
+            
+            
+          ];
+
+          shellHook = ''
+
+          '';
+        };
+      });
+}
